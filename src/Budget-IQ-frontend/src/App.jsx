97f532de712f810d36a3f5cd245c7de619f9a0b6
Judeike <ubongoudoh@gmail.com>
2024-09-06 10:14:52 +0100

    icons and tables

--- src/Budget-IQ-frontend/src/App.jsx
+++ src/Budget-IQ-frontend/src/App.jsx
@@ -4,9 +4,10 @@ import { Budget_IQ_backend } from 'declarations/Budget-IQ-backend';
 import Dasboard, { dashboardAction, dashboardLoader } from './pages/Dasboard';
 import Error from './pages/Error';
 import Main, { mainLoader } from './layout/Main';
-import { loggedoutAction } from './actions/logout';
+import { logoutAction } from './actions/logout';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ExpensesPage, { expensesLoader } from './pages/ExpensesPage';
 
 
 const router = createBrowserRouter([
@@ -17,15 +18,20 @@ const router = createBrowserRouter([
     errorElement:<Error />,
     children:[
       {
-        path: "/",
+        index: true,
         element:<Dasboard/>,
         loader : dashboardLoader,
         action: dashboardAction,
         errorElement:<Error />
       },
       {
+        path: "expenses",
+        element:<ExpensesPage/>,
+        loader : expensesLoader,
+      },
+      {
         path: "logout",
-        action: loggedoutAction
+        action: logoutAction
       },
     ]
   },
